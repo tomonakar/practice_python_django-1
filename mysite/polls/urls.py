9a955799bd7434d0cabbb7f97a9524db343ec5bb
tomonakar <tomohisa.nak@gmail.com>
2019-05-06 17:09:47 +0900

    feat: テンプレートシステムの利用 feat: urls.pyをテンプレートで呼び出す

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
+    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
