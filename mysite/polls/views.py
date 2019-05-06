4cbf1477288dbbd7ed49c8c33dc11a7c25a05bff
tomonakar <tomohisa.nak@gmail.com>
2019-05-06 16:47:29 +0900

    feat: templateを用いたviewの作成

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,31 @@
+# from django.template import loader
+from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
 
-def index(response):
-    return HttpResponse("Hello, world.")
+
+# この書き方には問題がある
+# ビューにデザインがハードコーディングされている
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+# 上の書き方をテンプレートを利用して書いたのが以下となる
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list': latest_question_list}
+#     return HttpResponse(template.render(context, request))
+
+# さらに上記をショートカットメソッド: render 利用して書き換えたの以下
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
