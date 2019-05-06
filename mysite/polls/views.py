ffbdd323939108e5be2a0037a824fceede69fa3f
tomonakar <tomohisa.nak@gmail.com>
2019-05-06 16:05:26 +0900

    feat: detail/results/vote view追加 feat: urls.pyを追記

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(response):
-  return HttpResponse("Hello, world.")
 
-# Create your views here.
+def index(response):
+    return HttpResponse("Hello, world.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on qustion %s." % question_id)
